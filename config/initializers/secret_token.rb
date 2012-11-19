6a8e2e03a528f864f0ab4c8ff315e0d2113ed758
sven@eventdrive.be <sven@eventdrive.be>
2012-11-19 16:44:00 +0100

    Initial commit of Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '05a62513e384b5418a1c1981d8cd605f6bb240e51eb813e9d0f8fca6427bf7c970b7e4807e5b5dc30f02b7abf80967beeb8ce9ce6d27d1724509f7d6ee51410e'
